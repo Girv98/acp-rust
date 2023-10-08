4716f2b9ddf57f4a951716987406f9d323d0abb3
James Girven <james.girven10@gmail.com>
2023-10-09 00:28:36 +0100

    Informal Output Testing

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,22 @@ mod ply;
 mod core;
 
 // use board::*;
-use crate::core::utils::pp_bb;
+use crate::core::{utils::pp_bb, Square};
 use game::*;
 // use position::*;
 
 fn main() {
-    let game = Game::new("".to_string());
+    let game = Game::new("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
     let bb = game.current_position().board.occupied_bb();
     println!("{}\n", pp_bb(bb));
-    game.current_position().board.print_board();
+
+    game.current_position().board.print_board(true);
     println!();
+    game.current_position().board.print_board(false);
+    println!();
+
+    println!("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
+    println!("{}", game.as_fen());
+
+    println!("{:?}", Square::from_bb(game.current_position().en_passant_targ.unwrap()));
 }
